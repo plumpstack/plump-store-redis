e906df4ce47ea7186d9cc075cab0d0460fd62a53
Eric Eslinger <eric.eslinger@gmail.com>
2017-02-21 13:46:22 -0800

    reconfiguring tests

--- gulp/config.js
+++ gulp/config.js
@@ -3,6 +3,6 @@ module.exports = {
   src: 'src',
   tests: 'test',
   types: 'types',
-  scripts: ['**/*.js'],
+  scripts: ['**/*.js', '!test/**'],
   typings: ['**/*.d.ts'],
 };
--- test/redis.spec.js
+++ test/redis.spec.js
@@ -1,8 +1,8 @@
 /* eslint-env node, mocha*/
 /* eslint no-shadow: 0 */
 
-import { RedisStore } from '../redis';
-import { testSuite } from 'plump';
+import { RedisStore } from '../src/redis';
+import { testSuite } from 'plump/test/storageTests';
 import * as Redis from 'fakeredis';
 import Bluebird from 'bluebird';
 

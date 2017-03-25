ccd5e81c7539f16c07ca8628a644ae88cb9e3111
Eric Eslinger <eric.eslinger@gmail.com>
2017-03-25 10:00:34 -0700

    new keyCounter in redis

--- test/redis.spec.ts
+++ test/redis.spec.ts
@@ -1,12 +1,12 @@
-/* eslint-env node, mocha*/
-/* eslint no-shadow: 0 */
-
 import { RedisStore } from '../src/redis';
 import { testSuite } from './storageTests';
+import { TestType } from './testType';
 import * as Redis from 'fakeredis';
 import * as Bluebird from 'bluebird';
 
 import 'mocha';
+import * as chai from 'chai';
+const expect = chai.expect;
 
 Bluebird.promisifyAll(Redis);
 
@@ -19,4 +19,18 @@ testSuite({
     terminal: true,
   },
   name: 'Plump Redis Store',
+});
+
+describe('Redis-specific functionality', () => {
+  it('should pre-allocate id values based on the store contents', () => {
+    const testClient = Redis.createClient();
+    const testStore = new RedisStore({ redisClient: testClient, terminal: true });
+    return testClient.setAsync(testStore.keyString({ typeName: TestType.typeName, id: 1 }), 'foo')
+    .then(() => testClient.setAsync(testStore.keyString({ typeName: TestType.typeName, id: 7 }), 'foo'))
+    .then(() => testStore.addSchema(TestType))
+    .then(() => testStore.allocateId(TestType.typeName))
+    .then((n) => expect(n).to.equal(8))
+    .then(() => testStore.allocateId(TestType.typeName))
+    .then((n) => expect(n).to.equal(9));
+  });
 });
